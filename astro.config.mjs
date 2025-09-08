5773e6e5acc1f58199a3a8ff7f06d9ebf8887358
tianium <36811828+tianium@users.noreply.github.com>
2025-09-08 12:27:52 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,24 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({ 
+export default defineConfig({
+    integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+      },
+    },
+  },
+  output: "static",
+  build: {
+    inlineStylesheets: "auto",
+  },
+  server: {
+    host: true,
+    port: 4321,
+  },
   site: 'https://tianium.github.io',
   base: '/page',
 });
